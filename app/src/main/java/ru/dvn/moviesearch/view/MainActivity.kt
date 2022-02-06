b6437f642e802b24e8f0f62ac2ffe092329157fe
Dmitriy Negrobov <ndv.def@gmail.com>
2022-02-06 19:12:16 +0300

    add onItemSelectListener

--- app/src/main/java/ru/dvn/moviesearch/view/MainActivity.kt
+++ app/src/main/java/ru/dvn/moviesearch/view/MainActivity.kt
@@ -2,6 +2,7 @@ package ru.dvn.moviesearch.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ru.dvn.moviesearch.R
 import ru.dvn.moviesearch.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -17,6 +18,28 @@ class MainActivity : AppCompatActivity() {
                 .beginTransaction()
                 .replace(binding.fragmentHost.id, HomeFragment.newInstance())
                 .commit()
+        }
+
+        binding.bottomNavigation.setOnItemSelectedListener {
+            when(it.itemId) {
+                R.id.bottom_item_home -> {
+                    supportFragmentManager
+                        .beginTransaction()
+                        .replace(binding.fragmentHost.id, HomeFragment.newInstance())
+                        .commit()
+                    true
+                }
+                R.id.bottom_item_favorites -> {
+                    supportFragmentManager
+                        .beginTransaction()
+                        .replace(binding.fragmentHost.id, FavoritesFragment.newInstance())
+                        .commit()
+                    true
+                }
+                else -> {
+                    false
+                }
+            }
         }
     }
 }
