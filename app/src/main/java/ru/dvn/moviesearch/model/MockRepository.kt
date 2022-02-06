69eba63e362c51e3eb8bc696402d310545d31545
Dmitriy Negrobov <ndv.def@gmail.com>
2022-02-06 16:29:23 +0300

    fix now playing movie item

--- app/src/main/java/ru/dvn/moviesearch/model/MockRepository.kt
+++ app/src/main/java/ru/dvn/moviesearch/model/MockRepository.kt
@@ -15,7 +15,8 @@ class MockRepository : Repository {
         return listOf(
             Movie("Эмма", "2020", 6.9, false),
             Movie("Вивариум", "2019", 5.7, false),
-            Movie("Воспоминания об убийстве", "2020", 8.1, false)
+            Movie("Воспоминания об убийстве", "2020", 8.1, false),
+            Movie("Почти знамениты", "2020", 7.2, false)
         )
     }
 }
