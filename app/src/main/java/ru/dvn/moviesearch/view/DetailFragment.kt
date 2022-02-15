99e9f8ed8113597e797b42bbc62e5d54bab48fd0
Dmitriy Negrobov <ndv.def@gmail.com>
2022-02-15 22:11:59 +0300

    Add detail fragment content

--- app/src/main/java/ru/dvn/moviesearch/view/DetailFragment.kt
+++ app/src/main/java/ru/dvn/moviesearch/view/DetailFragment.kt
@@ -1,11 +1,13 @@
 package ru.dvn.moviesearch.view
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import ru.dvn.moviesearch.R
 import ru.dvn.moviesearch.databinding.FragmentDetailBinding
 import ru.dvn.moviesearch.model.movie.Movie
 
@@ -24,6 +26,12 @@ class DetailFragment : Fragment() {
 
     private var _binding: FragmentDetailBinding? = null
     private val binding get() = _binding!!
+    private var movie: Movie? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        movie = arguments?.getParcelable(Movie::class.java.simpleName)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -31,15 +39,38 @@ class DetailFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         _binding = FragmentDetailBinding.inflate(inflater, container, false)
-        Log.d(
-            "TAG",
-            "Detail: ${this.id}"
-        )
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.name.text = movie?.name
+        binding.genre.text = movie?.genre
+
+        binding.favorite.setImageResource(
+            if (movie?.isFavorite == true) {
+                R.drawable.ic_favorite_for_user
+            } else {
+                R.drawable.ic_favorite_border
+            }
+        )
+
+
+
+        val length = "${getString(R.string.movie_length)} ${movie?.filmLength} min"
+        binding.length.text = length
+
+        val rating = "${getString(R.string.rating)} ${movie?.rating}"
+        binding.rating.text = rating
+
+        val budget = "${getString(R.string.budget)} ${movie?.budget}"
+        binding.budget.text = budget
+
+        val revenue = "${getString(R.string.revenue)} ${movie?.revenue}"
+        binding.revenue.text = revenue
+
+        val releaseDate = "${getString(R.string.release_date)} ${movie?.releaseDate}"
+        binding.releaseDate.text = releaseDate
     }
 
     override fun onDestroyView() {
